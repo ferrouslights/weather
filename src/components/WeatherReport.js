565e30e2cdc2c90610d4f5f692d5514c1b9b1edf
Benjamin Bridges <benjaminbridges.wayne@gmail.com>
2021-09-01 12:19:11 -0500

    Minor data processing tweaks

--- src/components/WeatherReport.js
+++ src/components/WeatherReport.js
@@ -1,7 +1,7 @@
 const WeatherReport = ({weatherData}) => {
     return (
         <div>
-            <h2>{weatherData.location.name}</h2>
+            <h2>{weatherData.location.name}, {weatherData.location.region.substring(0,2).toUpperCase()}</h2>
             <h2>{weatherData.current.temp_f}º</h2>
         </div>
     )
