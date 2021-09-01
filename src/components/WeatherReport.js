c33e27e206712411c27c234636d5926d029c822c
Benjamin Bridges <benjaminbridges.wayne@gmail.com>
2021-09-01 14:06:13 -0500

    Added weather icon

--- src/components/WeatherReport.js
+++ src/components/WeatherReport.js
@@ -46,6 +46,7 @@ const WeatherReport = ({weatherData}) => {
     return (
         <div>
             <h2>{weatherData.location.name}, {weatherData.location.region.substring(0,2).toUpperCase()}</h2>
+            <img src={'https:',weatherData.current.condition.icon}></img>
             <h2>{weatherData.current.temp_f}º</h2>
         </div>
     )
