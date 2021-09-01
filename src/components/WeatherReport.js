4eaba84844771620fdbddd20da9436d95576a2a2
Benjamin Bridges <benjaminbridges.wayne@gmail.com>
2021-09-01 12:16:53 -0500

    Added temp

--- src/components/WeatherReport.js
+++ src/components/WeatherReport.js
@@ -1,7 +1,8 @@
 const WeatherReport = ({weatherData}) => {
     return (
         <div>
-            <h2>Location: {weatherData.location.name}</h2>
+            <h2>{weatherData.location.name}</h2>
+            <h2>{weatherData.current.temp_f}º</h2>
         </div>
     )
 }
