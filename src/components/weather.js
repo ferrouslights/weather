b79898acd306dc4a164a24f229c59f9fefa813e8
Benjamin Bridges <benjaminbridges.wayne@gmail.com>
2021-09-02 22:15:21 -0500

    Improved api processing

--- src/components/weather.js
+++ src/components/weather.js
@@ -5,7 +5,7 @@ const apiKey = '55aed3d9628946fc92e143041212708'
 
 export const getWeatherData = async (cityname) => {
     try {
-        const {data} = await axios.get(baseUrl + '/current.json' + `?key=${apiKey}&q=${cityname}&aqi=no`)
+        const {data} = await axios.get(baseUrl + `/current.json?key=${apiKey}&q=${cityname}&aqi=no`)
         console.log(data);
         return data
     }
